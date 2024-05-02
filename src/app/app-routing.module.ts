579b2c83efd6b3e3d8cb22f0fc8779dc9f1a14dc
Nomfundo Mtshali <nomfundo.mtshali@investec.com>
2024-05-02 15:56:31 +0200

    adding page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { BookingPageComponent } from './booking-page/booking-page.component';
+import { AppointmentSearchComponent } from './appointment-search/appointment-search.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'booking-page', component: BookingPageComponent },
+  { path: 'appointemnt-Search', component: AppointmentSearchComponent },
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -33,7 +35,9 @@ import { AppointmentSearchComponent } from './appointment-search/appointment-sea
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatCardModule
+    MatCardModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
